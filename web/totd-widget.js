33c75113a8feb999c7d1dec1b6afb3a5775eaf83
Praminda Jayawardana <pramindabj@gmail.com>
2014-12-01 11:32:48 +0530

    Initial web widget commit

---
+++ web/totd-widget.js
@@ -0,0 +1,41 @@
+(function(){
+	var jQuery;
+
+	// load jquery if not present
+	if (window.jQuery === undefined || window.jQuery.fn.jquery !== '2.1.1') {
+	    var script_tag = document.createElement('script');
+	    script_tag.setAttribute("type","text/javascript");
+	    script_tag.setAttribute("src",
+	        "http://code.jquery.com/jquery-2.1.1.min.js");
+	    if (script_tag.readyState) {
+	      script_tag.onreadystatechange = function () { // For old versions of IE
+	          if (this.readyState == 'complete' || this.readyState == 'loaded') {
+	              scriptLoadHandler();
+	          }
+	      };
+	    } else { // Other browsers
+	      script_tag.onload = scriptLoadHandler;
+	    }
+	    // Try to find the head, otherwise default to the documentElement
+	    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+	} else {
+	    // The jQuery version on the window is the one we want to use
+	    jQuery = window.jQuery;
+	    main();
+	}
+
+	// called once jQuery has loaded
+	function scriptLoadHandler() {
+	    // Restore $ and window.jQuery to their previous values and store the
+	    // new jQuery in our local jQuery variable
+	    jQuery = window.jQuery.noConflict(true);
+	    // Call our main function
+	    main(); 
+	}
+
+	function main() { 
+	    jQuery(document).ready(function($) {
+	    	
+	    });
+	}
+})();
