de0dd572463885031e94ce2c82aa76cd767722fd
Yehan Pemarathne <yehancha@gmail.com>
2014-12-01 11:19:35 +0530

    Added initial db script

---
+++ db/tip.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2014 at 06:17 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tip_of_the_day`
+--
+
+CREATE DATABASE `tip_of_the_day`;
+
+--
+-- Table structure for table `tip`
+--
+
+CREATE TABLE IF NOT EXISTS `tip_of_the_day`.`tip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tip` varchar(255) NOT NULL,
+  `author` varchar(25) NOT NULL,
+  `hash` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
